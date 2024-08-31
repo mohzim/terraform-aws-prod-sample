c3c5f8aab1d9db0a007de97092af06cf124d72fc
mohzim <mohzim@gmail.com>
2024-09-01 00:56:01 +0530

    Add dummy s3 value for backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mohzim-terraform" # change this
+    bucket         = "abc-terraform" # change this
     key            = "terraform-aws-prod-sample/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
