8cc15020d070f6800d24e1fa216c51933b15c1f7
mohzim <mohzim@gmail.com>
2024-08-31 18:37:52 +0530

    Add secondary site

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 
 # Call application resources module to create resources in every region
-module "application-resources" {
+module "Primary-site-app-resources" {
     source = "./modules/app_resources"
     region = "ap-south-1"
+}
+
+module "Secondary-site-app-resources" {
+    source = "./modules/app_resources"
+    region = "ap-southeast-1"
 }
